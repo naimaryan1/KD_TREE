d7bb5a56f67865147527753415c39d56f2b2d26d
Naim <anaim@ndc.nasa.gov>
2020-10-22 16:48:42 -0700

    replaced JPL license with Apache License 2.0, approved by aryan.e.naim@jpl.nasa.gov

--- memory_allocation_test.c
+++ memory_allocation_test.c
@@ -1,22 +1,20 @@
- /*Copyright 2020, by the California Institute of Technology. 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+/* Copyright [2019-2020], by the California Institute of Technology. 
+ * ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged. 
+ * Any commercial use must be negotiated with the Office of Technology Transfer 
+ * at the California Institute of Technology.
+ * 
+ * This software may be subject to U.S. export control laws. By accepting this 
+ * software, the user agrees to comply with all applicable U.S. export laws and 
+ * regulations. User has the responsibility to obtain export licenses, or other 
+ * export authority as may be required before exporting such information to 
+ * foreign countries or providing access to foreign persons.
+
  * 
  * File:   memory_allocation_test.c
  * Author: anaim (aryan.e.naim@jpl.nasa.gov)
  * Created on August 4, 2020, 8:43 AM
  * 
- *IMPORTANT: Must call kdtree_alloc() & kdtree_init() once before using the API.
+ IMPORTANT: Must call kdtree_alloc() & kdtree_init() once before using the API.
  * In order to cleanup call kdtree_free(). 
  */
 
