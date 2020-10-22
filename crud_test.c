fa2b073bc1ec428444c1ac13e84f3b3b4a8ebe7b
Naim <anaim@ndc.nasa.gov>
2020-10-22 16:48:14 -0700

    replaced JPL license with Apache License 2.0, approved by aryan.e.naim@jpl.nasa.gov

--- crud_test.c
+++ crud_test.c
@@ -1,29 +1,30 @@
-
 #include <stdio.h>
 #include "kdtree.h"
 #include <sys/time.h>
 #include <math.h>
 #include <time.h> 
 
-/* Copyright [2019-2020], by the California Institute of Technology. 
- * ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged. 
- * Any commercial use must be negotiated with the Office of Technology Transfer 
- * at the California Institute of Technology.
+ /*Copyright 2020, by the California Institute of Technology. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
- * This software may be subject to U.S. export control laws. By accepting this 
- * software, the user agrees to comply with all applicable U.S. export laws and 
- * regulations. User has the responsibility to obtain export licenses, or other 
- * export authority as may be required before exporting such information to 
- * foreign countries or providing access to foreign persons.
-
- * File:   crud_test.c
+ * File:   kdtree.c
  * Author: anaim (aryan.e.naim@jpl.nasa.gov) 
- * Created on August 4, 2020, 7:45 AM
- * 
+ *
  * Example of libray usage  and test of add,search(single node_, delete, update 
  * and traverse. 
  * 
- * IMPORTANT: Must call kdtree_alloc() & kdtree_init() once before using the API. 
+ * IMPORTANT: Must call kdtree_alloc() & kdtree_init() once before using the API.
+ * 
  * In order to add points call kd_tree_add_points()
  * In order to update a point call kd_tree_update_point()
  * In order to search for a single point call kd_tree_search_data_point()
