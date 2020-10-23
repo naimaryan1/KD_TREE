5d51b15ec2693029e56f6ca72a69d4964a112bdc
Naim <anaim@ndc.nasa.gov>
2020-10-23 13:21:06 -0700

    updated readme files

--- batch_load_3d_points_for_test.c
+++ batch_load_3d_points_for_test.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include "kdtree.h"
-
-/*Copyright 2020, by the California Institute of Technology. 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/* Copyright [2019-2020], by the California Institute of Technology. 
+ * ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged. 
+ * Any commercial use must be negotiated with the Office of Technology Transfer 
+ * at the California Institute of Technology.
+ * 
+ * This software may be subject to U.S. export control laws. By accepting this 
+ * software, the user agrees to comply with all applicable U.S. export laws and 
+ * regulations. User has the responsibility to obtain export licenses, or other 
+ * export authority as may be required before exporting such information to 
+ * foreign countries or providing access to foreign persons.
  * 
  * Loads 3d points from a file for batch insertion into a kdtree. 
  * 
- * NOTE/TODO:This the upload script has CSV parsing bug as 10/22/2020, 
- * instead to test batch insertion refer to batch_test.c
+ * TODO:This the upload script has bug. To test batch insertion refer to 
+ * batch_test.c
  */
 
 void kd_tree_batch_load_points(char* file_path,
